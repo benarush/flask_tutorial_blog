7483421e8e26189f57fefa58aa56a4ab71ef7650
Tomer Ben haruah <tomerbenharuah@tomers-mbp.home>
2021-04-20 18:26:03 +0300

    set forms and its view, remeinds the templates

--- flask_blog.py
+++ flask_blog.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
+from .forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5fef642e91f5d0ff2fac29768c2ae3ab'
 
 posts = [
     {
@@ -27,6 +29,18 @@ def hello():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route('/registration')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', form=form, title="Register")
+
+
+@app.route('/login.html')
+def login():
+    form = LoginForm()
+    return render_template('login,html', form=form, title="login")
 
 if __name__ == '__main__':
     app.run(debug=True)
